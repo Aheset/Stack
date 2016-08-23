f12225972bec52c5a9b1a940122d06bd618b8bd2
Aheset <Aheset@gmail.com>
2016-08-23 00:09:34 -0400

    bugs fixed

--- myApp.java
+++ myApp.java
@@ -3,104 +3,56 @@ import java.util.Stack;
 
 public class myApp {
 
-
 	public static void main(String[] args) {
-		
-		Stack <Integer> s = new Stack<Integer>();
-		
-	/*	s.push(5);
-		s.push(15);
-		s.push(1);
-		
-		System.out.println(s.pop());
-		
-		//Dequeue takes elements out
-		//Enqueueputs in
-		 * 
-		 * 
-		 * 
-		 * int x = s.pop();
-		 * int y = s.pop();
-		 * 
-		 * 52*3+
-		 * 63-1+
-		 * 
-		*/
+
+		Stack<Integer> s = new Stack<Integer>();
+
 		int x = 0;
 		int y = 0;
 		int result = 0;
-		
+		int number;
+
 		Scanner scan1 = new Scanner(System.in);
 		System.out.println("please enter your string");
 		String userInput = scan1.next();
-	
+
 		for (int i = 0; i < userInput.length(); i++) {
-			  Character c = userInput.charAt(i);
-			  //System.out.println(c);
-			  
-			  if (c.isDigit(c)){
-				  int transcript = c.getNumericValue(c);
-				  s.push(transcript);
-			  }
-			 
-			  else {
-				 
-				  
-//				  System.out.println(x);
-//				  System.out.println(y);
-//			  
-			  
-			  switch (c){
-			  case '*':
-				  x = s.pop();
-				  y = s.pop();
-				  result = x * y;
-				  s.push(result);
-				  System.out.println(s.pop());
-				  break;
-				  
-			  case '/':
-				  x = s.pop();
-				  y = s.pop();
-				  result = x / y;
-				  s.push(result);
-				  System.out.println(s.pop());
-				  break;
-				  
-			  case '+':
-				  x = s.pop();
-				  y = s.pop();
-				  result = x + y;
-				  s.push(result);
-				  System.out.println(s.pop());
-				  break;
-				  
-			  case '-':
-				  x = s.pop();
-				  y = s.pop();
-				  result = x - y;
-				  s.push(result);
-				  System.out.println(s.pop());
-				  break;
-				  
-				  default: 
-					  break;
-			  }
-			  
-			 
-			  }
-			  }
+			Character c = userInput.charAt(i);
+			// System.out.println(c);
+
+			if(! (c.isDigit(c))) {
+			x = s.pop();
+			y = s.pop();
+				
+				switch (userInput.charAt(i)) {
+				case '*':
+					result = x * y;
+					s.push(result);
+					break;
+
+				case '/':
+					result = y/x;
+					s.push(result);
+					break;
+
+				case '+':
+					result = x + y;
+					s.push(result);
+					break;
+
+				case '-':
+					result = y -x;
+					s.push(result);
+					break;
+				}
+			}else{
+				number = Character.getNumericValue(userInput.charAt(i));
+				s.push(number);
+			//System.out.println(number);
+			}
 		}
+		System.out.println(result);
 	}
-
-			  
-
-/*
-String s = "...stuff...";
-
-for (int i = 0; i < s.length(); i++){
-    char c = s.charAt(i);        
-    //Process char
+	
 }
 
-*/
